17bf231b183592c1a51dcb113f5c17a9740fccf4
sudhakar17White <sudhakarkick17360@gmail.com>
2023-02-12 22:48:17 +0530

    redesign login and registor page,basic navigation

---
+++ Aluminiapp/app/src/main/java/com/example/aluminiapp/scroll.java
@@ -0,0 +1,14 @@
+package com.example.aluminiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class scroll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll);
+    }
+}
